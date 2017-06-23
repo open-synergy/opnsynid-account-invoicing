ebc04e90c739ae6d049bee6619637df06cf06a35
Andhitia Rama <andhitia.r@gmail.com>
2017-06-23 20:38:33 +0700

    [8.0.1.0.0][account_invoice_line_summary_by_product]

---
+++ account_invoice_line_summary_by_product/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Invoice Line Summary By Product",
+    "version": "8.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/account_invoice_views.xml",
+    ],
+}
---
+++ account_invoice_line_summary_by_product/models/account_invoice.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    line_summary_ids = fields.One2many(
+        string="Invoice Line Summary",
+        comodel_name="account.invoice_line_summary",
+        inverse_name="invoice_id",
+    )
