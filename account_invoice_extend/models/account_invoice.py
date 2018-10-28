decbe65f8b194d6c7a5614fa8cc36044bf8df323
mikevhe18 <viriyananda.michael@gmail.com>
2018-10-28 20:34:13 +0700

    [8.0.1.0.0][account_invoice_extend]

---
+++ account_invoice_extend/models/account_invoice.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    state = fields.Selection(
+        selection_add=[
+            ("confirm", "Confirm")
+        ]
+    )
