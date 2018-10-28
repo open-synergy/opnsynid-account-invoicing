572f05788bddf871d3869f326b06d1caa226119d
mikevhe18 <viriyananda.michael@gmail.com>
2018-10-28 20:34:43 +0700

    [8.0.1.0.0][account_invoice_tier_validation]

---
+++ account_invoice_tier_validation/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Invoice Tier Validation",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account_invoice_extend",
+        "base_tier_validation",
+    ],
+    "data": [
+        "views/account_invoice_views.xml"
+    ],
+}
