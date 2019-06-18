e08de481020c7c7aa0dabc2c86d0dd281d1c6521
mikevhe18 <viriyananda.michael@gmail.com>
2019-06-18 19:44:29 +0700

    [8.0.1.0.0][account_invoice_print_policy]

---
+++ account_invoice_print_policy/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Account Invoice Print Policy",
+    "version": "8.0.1.0.0",
+    "category": "Invoicing",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account",
+        "base_print_policy"
+    ],
+    "data": [
+        "security/res_groups.xml",
+        "views/account_invoice_views.xml",
+    ],
+}
