37ec43ab1d91b25dd51a03a3ec0d415bd192ed1a
mikevhe18 <viriyananda.michael@gmail.com>
2019-06-21 20:16:09 +0700

    [8.0.1.2.0][account_invoice_workflow_policy]

--- account_invoice_workflow_policy/models/account_journal.py
+++ account_invoice_workflow_policy/models/account_journal.py
@@ -2,7 +2,7 @@
 # Copyright 2017 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api
+from openerp import models, fields
 
 
 class AccountJournal(models.Model):
@@ -63,33 +63,3 @@ class AccountJournal(models.Model):
         column1="journal_id",
         column2="group_id",
     )
-
-    @api.model
-    def _get_invoice_workflow_button_policy_map(self):
-        return [
-            ("open_ok", "invoice_open_group_ids"),
-            ("refund_ok", "invoice_refund_group_ids"),
-            ("cancel_ok", "invoice_cancel_group_ids"),
-            ("restart_ok", "invoice_restart_group_ids"),
-            ("reopen_ok", "invoice_reopen_group_ids"),
-            ("send_email_ok", "invoice_send_email_group_ids"),
-            ("proforma_ok", "invoice_proforma_group_ids"),
-        ]
-
-    @api.multi
-    def _get_invoice_workflow_button_policy(self, policy_field):
-        self.ensure_one()
-        result = False
-        button_group_ids = []
-        user = self.env.user
-        group_ids = user.groups_id.ids
-
-        button_group_ids += getattr(
-            self, policy_field).ids
-
-        if not button_group_ids:
-            result = True
-        else:
-            if (set(button_group_ids) & set(group_ids)):
-                result = True
-        return result
