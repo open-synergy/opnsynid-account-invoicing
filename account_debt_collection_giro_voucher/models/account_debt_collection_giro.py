310de0ccac87c85229d9d0d7c3915c982f9ae827
mikevhe18 <viriyananda.michael@gmail.com>
2019-10-08 18:19:36 +0700

    [8.0.1.0.0][account_debt_collection_giro_voucher]

---
+++ account_debt_collection_giro_voucher/models/account_debt_collection_giro.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class AccountDebtCollectionGiro(models.Model):
+    _name = "account.debt_collection_giro"
+    _description = "Debt Collection Giro"
+    _inherit = [
+        "account.debt_collection_voucher_common",
+    ]
+
+    detail_ids = fields.One2many(
+        comodel_name="account.debt_collection_giro_detail",
+    )
+    giro_receipt_id = fields.Many2one(
+        string="# Giro Receipt",
+        comodel_name="account.giro_receipt",
+        ondelete="cascade",
+    )
+    allowed_journal_ids = fields.Many2many(
+        string="Allowed Giro Receipt Journals",
+        comodel_name="account.journal",
+        related="debt_collection_id.allowed_journal_giro_ids",
+        store=False,
+    )
+    date_issue = fields.Date(
+        string="Date Issued",
+        required=True,
+    )
+    date_due = fields.Date(
+        string="Date Due",
+        required=True,
+    )
+    source_bank_id = fields.Many2one(
+        string="Source Bank Account",
+        comodel_name="res.partner.bank",
+    )
+    destination_bank_id = fields.Many2one(
+        string="Destination Bank Account",
+        comodel_name="res.partner.bank",
+    )
+
+    @api.onchange(
+        "partner_id",
+    )
+    def onchange_source_bank_id(self):
+        obj_partner = self.env["res.partner"]
+        domain = {
+            "source_bank_id": [
+                ("id", "=", 0),
+            ]
+        }
+        self.source_bank_id = False
+        if self.partner_id:
+            commercial_partner = self.partner_id.commercial_partner_id
+            criteria = [
+                ("commercial_partner_id", "=", commercial_partner.id),
+            ]
+            partner_ids = obj_partner.search(criteria).ids
+            domain["source_bank_id"] = [
+                ("partner_id", "in", partner_ids),
+            ]
+
+        return {"domain": domain}
+
+    @api.onchange(
+        "partner_id",
+        "company_id",
+    )
+    def onchange_destination_bank_id(self):
+        obj_partner = self.env["res.partner"]
+        domain = {
+            "destination_bank_id": [
+                ("id", "=", 0),
+            ]
+        }
+        self.source_bank_id = False
+        if self.company_id and self.partner_id:
+            commercial_partner = self.company_id.partner_id.\
+                commercial_partner_id
+            criteria = [
+                ("commercial_partner_id", "=", commercial_partner.id),
+            ]
+            partner_ids = obj_partner.search(criteria).ids
+            domain["destination_bank_id"] = [
+                ("partner_id", "in", partner_ids),
+            ]
+
+        return {"domain": domain}
