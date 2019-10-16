55e00d600b63a8530e8b89c225235a84e5995144
mikevhe18 <viriyananda.michael@gmail.com>
2019-10-16 18:16:42 +0700

    [8.0.1.2.0][account_debt_collection_bank_cash_voucher]

--- account_debt_collection_bank_cash_voucher/__openerp__.py
+++ account_debt_collection_bank_cash_voucher/__openerp__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Account Debt Collection Bank and Cash",
-    "version": "8.0.1.1.0",
+    "version": "8.0.1.2.0",
     "category": "Invoicing",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
--- account_debt_collection_bank_cash_voucher/models/account_debt_collection_cash.py
+++ account_debt_collection_bank_cash_voucher/models/account_debt_collection_cash.py
@@ -2,7 +2,7 @@
 # Copyright 2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class AccountDebtCollectionCash(models.Model):
@@ -18,7 +18,7 @@ class AccountDebtCollectionCash(models.Model):
     cash_receipt_id = fields.Many2one(
         string="# Cash Receipt",
         comodel_name="account.cash_receipt",
-        ondelete="restrict",
+        ondelete="cascade",
     )
     allowed_journal_ids = fields.Many2many(
         string="Allowed Cash Receipt Journals",
@@ -26,3 +26,19 @@ class AccountDebtCollectionCash(models.Model):
         related="debt_collection_id.allowed_journal_cr_ids",
         store=False,
     )
+
+    @api.multi
+    def _check_cash_receipt_cancel(self):
+        self.ensure_one()
+        result = True
+        obj_cash_receipt =\
+            self.env["account.cash_receipt"]
+        if self.cash_receipt_id:
+            criteria = [
+                ("state", "not in", ["draft", "cancel"]),
+                ("id", "=", self.cash_receipt_id.id)
+            ]
+            post_count = obj_cash_receipt.search_count(criteria)
+            if post_count > 0:
+                result = False
+        return result
