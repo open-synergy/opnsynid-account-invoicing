0ff104cbf11e054728158a1f6b8fc40bebf699c6
mikevhe18 <viriyananda.michael@gmail.com>
2019-10-16 18:18:06 +0700

    [8.0.1.1.0][account_debt_collection_giro_voucher]

--- account_debt_collection_giro_voucher/models/account_debt_collection.py
+++ account_debt_collection_giro_voucher/models/account_debt_collection.py
@@ -2,7 +2,8 @@
 # Copyright 2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api
+from openerp import models, fields, api, _
+from openerp.exceptions import Warning as UserError
 
 
 class AccountDebtCollection(models.Model):
@@ -43,6 +44,19 @@ class AccountDebtCollection(models.Model):
     )
 
     @api.multi
+    def action_cancel(self):
+        _super = super(AccountDebtCollection, self)
+        result = _super.action_cancel()
+        for document in self:
+            for giro_detail in document.giro_detail_ids:
+                if not giro_detail._check_giro_receipt_cancel():
+                    msg = _("Giro Receipts must be on <Draft> state")
+                    raise UserError(msg)
+                giro_detail.giro_receipt_id.unlink()
+            document.giro_detail_ids.unlink()
+        return result
+
+    @api.multi
     def _prepare_giro_receipt_data(self, document):
         self.ensure_one()
 
@@ -66,35 +80,56 @@ class AccountDebtCollection(models.Model):
         obj_giro_receipt_line =\
             self.env["account.giro_receipt_line"]
 
-        for giro in self.giro_detail_ids:
-            voucher_data =\
-                self._prepare_receipt_voucher_data(
-                    giro, voucher_type_id)
-            giro_data =\
-                self._prepare_giro_receipt_data(giro)
-            giro_data.update(voucher_data)
-            gr = obj_giro_receipt.create(giro_data)
-            giro.write({
-                "giro_receipt_id": gr.id
-            })
-            for giro_detail in giro.detail_ids:
-                giro_line = obj_giro_receipt_line.create(
-                    self._prepare_receipt_voucher_line_data(
-                        gr, giro_detail))
-                giro_detail.write({
-                    "giro_receipt_line_id": giro_line.id
-                })
-        return gr
+        if self.giro_detail_ids:
+            for giro in self.giro_detail_ids:
+                if giro.detail_ids:
+                    voucher_data =\
+                        self._prepare_receipt_voucher_data(
+                            giro, voucher_type_id)
+                    giro_data =\
+                        self._prepare_giro_receipt_data(giro)
+                    giro_data.update(voucher_data)
+                    gr = obj_giro_receipt.create(giro_data)
+                    giro.write({
+                        "giro_receipt_id": gr.id
+                    })
+                    for giro_detail in giro.detail_ids:
+                        giro_line = obj_giro_receipt_line.create(
+                            self._prepare_receipt_voucher_line_data(
+                                gr, giro_detail))
+                        giro_detail.write({
+                            "giro_receipt_line_id": giro_line.id
+                        })
+        return True
 
     @api.multi
     def action_done(self):
         _super = super(AccountDebtCollection, self)
         result = _super.action_done()
         for document in self:
-            giro_detail_ids =\
-                document.giro_detail_ids
-            detail_giro =\
-                document.giro_detail_ids.detail_ids
-            if giro_detail_ids and detail_giro:
-                document._create_giro_receipt()
+            document._create_giro_receipt()
         return result
+
+    @api.multi
+    def _get_giro_receipts(self):
+        return self.mapped("giro_detail_ids.giro_receipt_id")
+
+    @api.multi
+    def _get_action_giro_receipt(self):
+        action =\
+            self.env.ref(
+                "account_voucher_giro."
+                "account_giro_receipt_action").read()[0]
+        return action
+
+    @api.multi
+    def action_view_giro_receipts(self):
+        self.ensure_one()
+        receipts = self._get_giro_receipts()
+        action = self._get_action_giro_receipt()
+
+        if len(receipts) > 0:
+            action["domain"] = [("id", "in", receipts.ids)]
+        else:
+            action = {"type": "ir.actions.act_window_close"}
+        return action
