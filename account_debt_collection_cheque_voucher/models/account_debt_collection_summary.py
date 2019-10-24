e7f2c796bf8988272d0d9d016274989b2acb9e7c
mikevhe18 <viriyananda.michael@gmail.com>
2019-10-24 19:19:16 +0700

    [8.0.1.3.0][account_debt_collection_cheque_voucher]

---
+++ account_debt_collection_cheque_voucher/models/account_debt_collection_summary.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+import openerp.addons.decimal_precision as dp
+
+
+class AccountDebtCollectionDetailSummary(models.Model):
+    _inherit = "account.debt_collection_detail_summary"
+
+    total_amount_cheque_collected = fields.Float(
+        string="Total Collected Cheque Amount",
+        digits=dp.get_precision("Account"),
+    )
+
+    def _select(self):
+        _super = super(AccountDebtCollectionDetailSummary, self)
+        select_str = _super._select() + """,
+            SUM(a.amount_cheque_collected) AS total_amount_cheque_collected
+        """
+        return select_str
