6621444eef99a832f7d8a87c94bfc781ef77e8b2
mikevhe18 <viriyananda.michael@gmail.com>
2019-10-24 19:18:43 +0700

    [8.0.1.4.0][account_debt_collection_bank_cash_voucher]

--- account_debt_collection_bank_cash_voucher/__openerp__.py
+++ account_debt_collection_bank_cash_voucher/__openerp__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Account Debt Collection Bank and Cash",
-    "version": "8.0.1.3.0",
+    "version": "8.0.1.4.0",
     "category": "Invoicing",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
---
+++ account_debt_collection_bank_cash_voucher/models/account_debt_collection_summary.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+import openerp.addons.decimal_precision as dp
+
+
+class AccountDebtCollectionDetailSummary(models.Model):
+    _inherit = "account.debt_collection_detail_summary"
+
+    total_amount_bank_collected = fields.Float(
+        string="Total Collected Bank Amount",
+        digits=dp.get_precision("Account"),
+    )
+
+    total_amount_cash_collected = fields.Float(
+        string="Total Collected Cash Amount",
+        digits=dp.get_precision("Account"),
+    )
+
+    def _select(self):
+        _super = super(AccountDebtCollectionDetailSummary, self)
+        select_str = _super._select() + """,
+            SUM(a.amount_bank_collected) AS total_amount_bank_collected,
+            SUM(a.amount_cash_collected) AS total_amount_cash_collected
+        """
+        return select_str
