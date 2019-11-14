2a4e42c27bda6d6709d5d1042415fdccfc79f57c
mikevhe18 <viriyananda.michael@gmail.com>
2019-11-14 13:03:46 +0700

    [8.0.1.4.0][account_debt_collection_giro_voucher] Issue: #32

---
+++ account_debt_collection_giro_voucher/models/account_debt_collection_summary_by_date.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+import openerp.addons.decimal_precision as dp
+
+
+class AccountDebtCollectionSummaryByDate(models.Model):
+    _inherit = "account.debt_collection_summary_by_date"
+
+    total_amount_giro_collected = fields.Float(
+        string="Total Collected Giro Amount",
+        digits=dp.get_precision("Account"),
+    )
+
+    def _select(self):
+        _super = super(AccountDebtCollectionSummaryByDate, self)
+        select_str = _super._select() + """,
+            SUM(a.amount_giro_collected) AS total_amount_giro_collected
+        """
+        return select_str
