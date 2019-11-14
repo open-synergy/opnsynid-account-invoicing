091ea167d1c46021b7f2d620020d09ab61387649
mikevhe18 <viriyananda.michael@gmail.com>
2019-11-14 13:04:07 +0700

    [8.0.1.4.0][account_debt_collection_cheque_voucher] Issue: #32

--- account_debt_collection_cheque_voucher/__openerp__.py
+++ account_debt_collection_cheque_voucher/__openerp__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Account Debt Collection Cheque",
-    "version": "8.0.1.3.0",
+    "version": "8.0.1.4.0",
     "category": "Invoicing",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
@@ -16,6 +16,7 @@
     "data": [
         "security/ir.model.access.csv",
         "views/account_debt_collection_views.xml",
+        "views/account_debt_collection_summary_by_date_views.xml",
     ],
     "installable": True,
     "auto_install": True,
