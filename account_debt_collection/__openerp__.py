fe0115c6bb6d8ee88cd07214cd71ccee35d3e0dd
Andhitia Rama <andhitia.r@gmail.com>
2019-12-09 20:46:09 +0700

    [8.0.1.6.0] account_debt_collection

--- account_debt_collection/__openerp__.py
+++ account_debt_collection/__openerp__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Account Debt Collection",
-    "version": "8.0.1.5.0",
+    "version": "8.0.1.6.0",
     "category": "Invoicing",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
