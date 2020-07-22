5c6b217eb468efdf571b1e77c242e4c099d7a90a
mikevhe18 <viriyananda.michael@gmail.com>
2020-07-22 15:02:31 +0700

    [8.0.1.0.0][account_invoice_multiple_approval_workflow_policy]

---
+++ account_invoice_multiple_approval_workflow_policy/models/account_invoice.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from openerp import models, fields, api
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    @api.multi
+    @api.depends(
+        "journal_id",
+    )
+    def _compute_policy(self):
+        _super = super(AccountInvoice, self)
+        _super._compute_policy()
+
+    confirm_ok = fields.Boolean(
+        string="Can Confirm",
+        compute="_compute_policy",
+        store=False,
+    )
+    restart_approval_ok = fields.Boolean(
+        string="Can Restart Approval",
+        compute="_compute_policy",
+        store=False,
+    )
