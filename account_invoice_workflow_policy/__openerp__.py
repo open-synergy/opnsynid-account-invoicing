663e2832ffe4532962f264b54aa1f6ba2b3e2d5c
Andhitia Rama <andhitia.r@gmail.com>
2020-10-01 14:08:51 +0700

    [account_invoice_workflow_policy] rebrand

--- account_invoice_workflow_policy/__openerp__.py
+++ account_invoice_workflow_policy/__openerp__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Account Invoice Workflow Policy",
     "version": "8.0.1.2.0",
     "category": "Accounting",
-    "website": "https://opensynergy-indonesia.com",
-    "author": "OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia,OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
@@ -18,5 +19,8 @@
         "data/base_workflow_policy_data.xml",
         "views/account_journal_views.xml",
         "views/account_invoice_views.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
     ],
 }
