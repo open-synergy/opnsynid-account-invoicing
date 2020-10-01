784e150df6ebfd641949314e134a346b4247906f
Andhitia Rama <andhitia.r@gmail.com>
2020-10-01 14:13:10 +0700

    [account_invoice_workflow_policy_payment] 8.0.1.2.0

--- account_invoice_workflow_policy_payment/models/__init__.py
+++ account_invoice_workflow_policy_payment/models/__init__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 OpenSynergy Indonesia
+# Copyright 2020 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import account_journal
-from . import account_invoice
+from . import (
+    account_journal,
+    account_invoice,
+)
