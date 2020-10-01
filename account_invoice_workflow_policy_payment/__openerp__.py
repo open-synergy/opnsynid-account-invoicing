7d6d22a2a17ef398f98f90f9e3281dfab5276eb5
Andhitia Rama <andhitia.r@gmail.com>
2020-10-01 14:13:10 +0700

    [account_invoice_workflow_policy_payment] 8.0.1.2.0

--- account_invoice_workflow_policy_payment/__openerp__.py
+++ account_invoice_workflow_policy_payment/__openerp__.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
+# Copyright 2020 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Account Invoice Workflow Policy - "
             "account_voucher Integration",
-    "version": "8.0.1.1.0",
+    "version": "8.0.1.2.0",
     "category": "Accounting",
-    "website": "https://opensynergy-indonesia.com",
-    "author": "OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "auto_install": True,
@@ -21,5 +22,8 @@
         "data/base_workflow_policy_data.xml",
         "views/account_journal_views.xml",
         "views/account_invoice_views.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
     ],
 }
