740fd47cd8669acb37a01e7716cb84f19019d09b
mikevhe18 <viriyananda.michael@gmail.com>
2021-06-28 09:04:23 +0700

    [account_invoice_sequence_configurator] 8.0.1.0.0
    Fix #45

---
+++ account_invoice_sequence_configurator/__openerp__.py
@@ -0,0 +1,25 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Invoicing Sequence Configurator",
+    "summary": "Implement a sequence configurator process " "for Invoicing",
+    "version": "8.0.1.0.0",
+    "category": "Invoicing",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "account",
+        "base_sequence_configurator",
+    ],
+    "data": [
+        "data/ir_sequence_data.xml",
+        "data/base_sequence_configurator_data.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+}
