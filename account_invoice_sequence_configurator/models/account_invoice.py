ec7b332848c8b9935c3548ab525ec3aff72e35cb
mikevhe18 <viriyananda.michael@gmail.com>
2021-06-28 09:04:23 +0700

    [account_invoice_sequence_configurator] 8.0.1.0.0
    Fix #45

---
+++ account_invoice_sequence_configurator/models/account_invoice.py
@@ -0,0 +1,34 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from openerp import api, fields, models
+
+
+class AccountInvoice(models.Model):
+    _name = "account.invoice"
+    _inherit = [
+        "account.invoice",
+        "base.sequence_document",
+    ]
+
+    @api.model
+    def _default_internal_number(self):
+        return "/"
+
+    internal_number = fields.Char(
+        default=lambda self: self._default_internal_number(),
+    )
+
+    @api.multi
+    def action_move_create(self):
+        _super = super(AccountInvoice, self)
+        ctx = self.env.context.copy()
+        for document in self:
+            ctx.update(
+                {
+                    "ir_sequence_date": document.date_invoice,
+                }
+            )
+            sequence = document.with_context(ctx)._create_sequence()
+            document.write({"internal_number": sequence})
+        return _super.action_move_create()
