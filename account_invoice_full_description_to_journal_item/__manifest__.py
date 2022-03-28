c9c310be5644cf7f32293597e90408695ebf36ec
Andhitia Rama <andhitia.r@gmail.com>
2022-03-29 05:32:09 +0700

    [account_invoice_full_description_to_journal_item] 11.0.1.0.0

---
+++ account_invoice_full_description_to_journal_item/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 OpenSynergy Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Full Invoice Line Description To Journal Item",
+    "version": "11.0.1.0.0",
+    "license": "LGPL-3",
+    "category": "Invoicing",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "depends": [
+        "account",
+        "ssi_source_document_mixin",
+    ],
+    "data": [],
+    "installable": True,
+    "auto_install": False,
+}
