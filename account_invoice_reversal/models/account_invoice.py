ab9d4c8d5f451523e43ed450c03a0a4d05ec8f5a
Andhitia Rama <andhitia.r@gmail.com>
2022-04-18 15:40:24 +0700

    [account_invoice_reversal] 11.0.1.1.0
    * Add paid_status to differentiate between invoice that reversed or actually paid

--- account_invoice_reversal/models/account_invoice.py
+++ account_invoice_reversal/models/account_invoice.py
@@ -1,12 +1,25 @@
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AccountInvoice(models.Model):
     _name = "account.invoice"
     _inherit = ["account.invoice"]
+
+    @api.depends(
+        "state",
+        "reversal_id",
+    )
+    def _compute_paid_status(self):
+        for record in self:
+            result = False
+            if record.state == "paid" and record.reversal_id:
+                result = "reverse"
+            elif record.state == "paid" and not record.reversal_id:
+                result = "paid"
+            record.paid_status = result
 
     reversal_id = fields.Many2one(
         string="Reversal Entry",
@@ -14,4 +27,13 @@ class AccountInvoice(models.Model):
         related="move_id.reversal_id",
         store=True,
         readonly=False,
+    )
+    paid_status = fields.Selection(
+        string="Paid Status",
+        selection=[
+            ("paid", "Paid"),
+            ("reverse", "Reversed"),
+        ],
+        compute="_compute_paid_status",
+        store=True,
     )
